045370dba1960b6816f0c038505e7a390ec4d641
MireyaJO <miregarcia444@gmail.com>
2024-06-28 11:23:21 -0500

    version 2- García Mireya

--- src/login.java
+++ src/login.java
@@ -4,28 +4,30 @@ import java.awt.event.ActionListener;
 import java.util.Scanner;
 public class login {
     private JTextField nombreu;
-    private JButton BORRAR;
     private JButton OK;
     private JLabel nombre;
     private JLabel contraseña;
     private JPasswordField passwordField1;
     public JPanel login;
-    private JLabel problema;
     private JTextField contrase;
-
-
-    public login() {
+    public login(JFrame frame1) {
         OK.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String nombre = "Mireya Garcia";
                 String contrasenia ="Nena";
-                if (nombreu==null || contrase==null){
-                    problema.setText("ERROR NO SE HAN INGRESADO DATOS");
-                } else if (nombreu.equals(nombre)){
+                if (nombreu.equals(nombre)){
                     System.out.println("El nombre es incorrecto");
                 } else if (contrase.equals(contrasenia)){
-                    System.out.println("El contrase esta incorrecta");
+                    System.out.println("La contraseña esta incorrecta");
+                } else if (nombreu.equals(nombre) &&  contrase.equals(contrasenia)){
+                    JFrame frame = new JFrame("Nueva ventana");
+                    frame.setContentPane(new  Bienvenida().VentanaNueva);
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setSize(500, 500);
+                    frame.pack();
+                    frame.setVisible(true);
+                    frame1.setVisible(false);
                 }
             }
         });
